3c0df9cb69d5d490b9432e7f7373bddc35e80158
Marcelo Motta <128328762+mottamarcelo@users.noreply.github.com>
2025-01-18 09:46:19 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\motta\OneDrive\Documentos\file1.txt";
+            string targetPath = @"C:\Users\motta\OneDrive\Documentos\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+                        double total = price * quantity;
+                        sw.WriteLine(name + ',' + total.ToString("F2", CultureInfo.InvariantCulture));
+                        Console.WriteLine(name + ',' + total.ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+
+}
